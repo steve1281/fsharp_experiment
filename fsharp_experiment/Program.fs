ae7bcc0974ed48548ae1643921086d0b42d682ba
steven.falcigno <steven.falcigno@heakthefx.us>
2017-04-03 17:03:20 -0400

    initial

---
+++ fsharp_experiment/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Markup
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv = 
+    let mainWindow = Application.LoadComponent(
+                        new System.Uri("fsharp_experiment;component/MainWindow.xaml", UriKind.Relative)) :?> Window
+    let application = new Application()
+    application.Run(mainWindow)
